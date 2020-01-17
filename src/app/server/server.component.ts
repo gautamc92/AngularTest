ce564c3245ee19cb6c166e3a9391eef1dd9d33cc
Gautam Chopda <chopdagautam92@gmail.com>
2020-01-17 12:17:47 +0530

    First  Commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({    
+    selector : 'server-root',
+    templateUrl: './server.component.html',
+    styleUrls : ['./server.component.css']
+})
+export class ServerComponent{
+
+}
